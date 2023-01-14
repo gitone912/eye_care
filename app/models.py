99f330a5451f1447dee477bb67231f61ad5afe1b
gitone912 <96557682+gitone912@users.noreply.github.com>
2023-01-14 15:52:43 +0530

    models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+selection = (
+    ('m', 'male'),
+    ('f', 'female'),
+    )
+class user_details(models.Model):
+    name = models.CharField(max_length=60, blank=True, default='',verbose_name="name" )
+    age = models.IntegerField(blank=True, default='',verbose_name="age" )
+    gender = models.CharField(max_length=60, blank=True, default='',choices=selection,verbose_name="status" )
+    mail_id = models.EmailField(max_length=60, blank=True, default='',verbose_name="mail_id" )
+    image1 = models.ImageField(upload_to='images/', blank=True, default='',verbose_name="images" )
+    image2 = models.ImageField(upload_to='images/', blank=True, default='',verbose_name="images" )
+    
